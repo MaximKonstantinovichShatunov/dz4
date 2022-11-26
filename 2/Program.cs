388cb2d42e7d9bb6c22c4f0b2b6851af7c330c1f
maxim <shatunov-87@mail.ru>
2022-11-26 23:39:34 +0300

    DZ4

---
+++ 2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.WriteLine("Введите число  ");
+int Namber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма всех цифр в числе = {summaNambersInNamber(Namber)}");
+int summaNambersInNamber(int N)
+{
+    int sum = 0;
+    while (N > 0)
+    {
+        sum = sum + N % 10;
+        N = N / 10;
+    }
+    return sum;
+}
+
+
+
---
+++ 3/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]*/
+
+
+
+void PrintArray(int[] Array)
+{
+    for (int i=0; i < 8 ; i++){
+
+        Console.Write($"{Array[i]}");
+    }
+}
+
+Console.WriteLine("Введите 8 чисел: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+int number4 = Convert.ToInt32(Console.ReadLine());
+int number5 = Convert.ToInt32(Console.ReadLine());
+int number6 = Convert.ToInt32(Console.ReadLine());
+int number7 = Convert.ToInt32(Console.ReadLine());
+int number8 = Convert.ToInt32(Console.ReadLine());
+int[] Arr= new int[] {number1, number2, number3, number4, number5,number6, number7, number8};
+
+PrintArray(Arr);
